078ecb0b07a8f18832ca30665de943466f492071
Matthew Nielsen <xunker@pyxidis.org>
2014-03-06 21:39:13 -0700

    added cors:wq

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  after_filter :cors_set_access_control_headers
+
   def current_station
     @current_station ||= begin
       params.require(:station)
@@ -22,5 +24,13 @@ protected
   def respond_with_success(options = {})
     respond_with( { status: :ok }.merge(options), status: :ok, location: nil)
   end
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin']      = '*'
+    headers['Access-Control-Allow-Methods']     = 'GET, OPTIONS'
+    headers['Access-Control-Max-Age']           = '1728000'
+    headers['Access-Control-Allow-Credentials'] = 'true'
+  end
+
 
 end
