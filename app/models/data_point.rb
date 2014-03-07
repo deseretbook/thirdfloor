07c574e155a172966b68315af0bb94b6b934f1f6
Matthew Nielsen <xunker@pyxidis.org>
2014-03-07 12:46:23 -0700

    Changed data retention rules: 1 week for user locations, 2 weeks for data_points.

--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -10,7 +10,7 @@ class DataPoint < ActiveRecord::Base
   def delete_old_records
     # only do this ~20% of the time
     if rand(10) >= 8
-      DataPoint.where(['created_at < ?', 1.month.ago]).delete_all
+      DataPoint.where(['created_at < ?', 2.weeks.ago]).delete_all
     end
   end
 
