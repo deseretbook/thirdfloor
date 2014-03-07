0aab2ff2916807e88f6fc1b18a148c0bbeb4f896
Matthew Nielsen <xunker@pyxidis.org>
2014-03-06 20:18:10 -0700

    delete old user_location records

--- app/models/user_location.rb
+++ app/models/user_location.rb
@@ -4,9 +4,21 @@ class UserLocation < ActiveRecord::Base
 
   default_scope -> { order('id') }
 
+  after_save :delete_old_records
+
   # true if created_at is after 8am
   def in_office_today?
     @in_office_today ||= created_at > Time.parse('8am')
+  end
+
+private
+
+  # doing this here saves managing a cron job
+  def delete_old_records
+    # only do this ~20% of the time
+    if rand(10) >= 8
+      UserLocation.where(['created_at < ?', 1.month.ago]).delete_all
+    end
   end
 
 end
