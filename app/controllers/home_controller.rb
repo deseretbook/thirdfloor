05c61cd8d469abb86d58b66a0fcc6438c1962b43
Matthew Nielsen <xunker@pyxidis.org>
2014-03-11 16:41:11 -0600

    removed un-needed @virtual_path stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,9 @@
 class HomeController < ApplicationController
   def visualize
     if vis = Visualization.find_by_slug(params[:slug])
-      @virtual_path = "visualize/#{params[:slug]}"
       render(
         inline: vis.markup,
         type: vis.markup_type,
-        virtual_path: @virtual_path,
         locals: { visualization: vis }
       )
     else
