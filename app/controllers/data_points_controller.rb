e0c81b5af466e62fbd1c9aecfd8e0abc7fba14bb
Matthew Nielsen <xunker@pyxidis.org>
2014-03-18 16:42:20 -0600

    removed login required until we figure out a better way to handle logins.

--- app/controllers/data_points_controller.rb
+++ app/controllers/data_points_controller.rb
@@ -3,7 +3,7 @@ class DataPointsController < ApplicationController
 
   skip_before_filter :verify_authenticity_token, only: [ :create, :named_route_create ]
 
-  before_filter :login_required, only: [ :index, :show ]
+  # before_filter :login_required, only: [ :index, :show ]
 
   def index # GET collection
     respond_with(data_points: cached_data_points) do |format|
