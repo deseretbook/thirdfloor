6ca9fedfad38c594d16dca1ceecae3d9d8bc8257
Matthew Nielsen <xunker@pyxidis.org>
2014-04-08 16:42:53 -0600

    Add deployment fanfare to home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def render_visualization(slug)
+    if vis = Visualization.find_by_slug(slug)
+      render inline: vis.markup, type: vis.markup_type
+    else
+      render text: "Visualization '#{slug}' not found."
+    end
+  end
 end
